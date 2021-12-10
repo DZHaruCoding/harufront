3a5c81a6a46e4428f211da115289608b63e73036
kimhaengun <chxl89@naver.com>
2021-12-10 19:28:32 +0900

    #김행운 [add]:내 프로젝트 리스트 뿌리기

---
+++ src/components/event/MyProject.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Member from './Member';
+
+const Project = ({project}) => {
+  const members = project.members;
+    return(
+        <div body className="position-relative  pb-5 w-100">
+          <div style={{flex:1}}>
+          <h6 className="fs-0 mb-1 " style={{color:"red"}}>
+            제목 : {' '}
+            {/* <Button className="ml-5">수정</Button> */}
+            {project.projectTitle}
+          </h6>
+          </div>
+
+          <div style={{flex:1}}>
+          <p className="mb-1">
+           내용 : {' '}
+              {project.projectDesc}
+          </p>
+          </div>
+          <p className="mb-1">
+           시작일 :{' '}
+              {project.projectStart}
+          </p>
+          <p className="mb-1">
+           마감일 :{' '}
+              {project.projectEnd}
+          </p>
+          <p className="mb-1">
+           멤버 :{' '}
+              {
+                members.map( member => <Member member={member}/>)
+              }
+          </p>
+    
+        </div>  
+      );    
+    };
+
+export default Project;
