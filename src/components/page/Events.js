2dec46197a2ca74c9f36d057858e05fa16235889
kimhaengun <chxl89@naver.com>
2021-12-15 22:49:34 +0900

    #김행운[add]:프로젝트 생성 완료

--- src/components/page/Events.js
+++ src/components/page/Events.js
@@ -14,7 +14,7 @@ import styles from './modal.scss';
 import { flexibleCompare } from '@fullcalendar/react';
 import Datetime from 'react-datetime';
 import { Link } from 'react-router-dom';
-
+import { offline } from 'is_js';
 
 
 const Events = () => {
@@ -26,6 +26,7 @@ const Events = () => {
   const [endDate, setEndDate] = useState();
   const [startDate, setStartDate] = useState();
 
+  const [rend, setRend] = useState(false);
 
   const handleChange = target => {
     let name = target.name;
@@ -66,8 +67,132 @@ const Events = () => {
     }
   }
   data();
-  },[]);
+  },[rend]);
   
+  //멤버 인풋
+  const [memberInputOpen, setMemberInputOpen] = useState(false);
+  const [searchMembers, setSearchMembers] = useState(); 
+
+  const memberSearchandInput = () => {    
+    const fetchfun = async () => {
+      try {
+        const response = await fetch(`${localIp}/api/project/member`,{
+          method: 'get',
+          headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: null
+        });
+
+        console.log('프로젝트 생성 멤버 찾기 @: '+response);
+
+
+      if(!response.ok){
+          throw new Error(`${response.status} ${response.statusText}`);
+      }
+      //response 데이터 json으로 변환하기
+      const jsonResult = await response.json(); //여기서부터 오류 null값..
+     
+      console.log('프로젝트 생성 멤버 찾기 json data :',jsonResult.data);
+
+            //통신 했지만 결과값이 success가 아니면
+      if(jsonResult.result !== 'success'){
+      throw new Error(`${jsonResult.result} ${jsonResult.message}`);
+      }
+      setSearchMembers(jsonResult.data);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchfun();
+    setMemberInputOpen(true);
+  }
+  //키워드
+  const [keyword,setKeyword] = useState('');
+  //선택 된 멤버 담는 상태값
+  const [mSelects, setMSelects] = useState([]);
+  //멤버 클릭 시
+  const memberselect = (userNo,userName,userEmail,userPhoto) =>{
+    console.log("클릭된 유저 no :",userNo,userName,userEmail,userPhoto)
+    const data = {
+      userNo: userNo,
+      userName: userName,
+      userEmail: userEmail,
+      userPhoto: userPhoto
+    }
+
+    //멤버 클릭 중복 제거
+    let a = false;
+    for(let i = 0; i<mSelects.length; i++){
+      if(data.userNo == mSelects[i].userNo){
+        
+        a = true;
+        return;
+      }
+    }
+    if(a==true){
+      return;
+    }
+    setMSelects([...mSelects, data]);
+  }
+
+  const modalFalse = () =>{
+    setModalIsOpen(false)
+    setSearchMembers()
+    setKeyword('');
+    setMSelects([]);
+    setMemberInputOpen(false);
+  }
+  //프로젝트 모달창 input태그 오브젝트로 변환
+  const [modalformObj, setModalFormObj] = useState();
+  const modalhandleChange = (target) =>{
+    let name = target.name;
+    let value = name === 'allDay' ? target.checked : target.value;
+    setModalFormObj({ ...modalformObj, [name]: value });
+  }
+
+  //프로젝트 생성 클릭 시
+  const projectAdd = () =>{
+    console.log('modalobj:',modalformObj)
+    //members
+    console.log('finalobj',mSelects);
+
+
+    modalformObj.members = mSelects;
+    console.log(modalformObj)
+    // const members = {
+
+    //   members:[mSelects]
+    // }
+
+    // console.log('만든 members',members);
+    // setModalFormObj({...modalformObj, members});
+    console.log(modalformObj);
+    const fetchfun = async() => {
+        const response = await fetch(`${localIp}/api/project/add/1`,{
+          method:"post",
+          headers:{
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+          },
+          body:JSON.stringify(modalformObj)
+        });
+        if(!response.ok){
+          throw new Error(`${response.status} ${response.statusText}`);
+      }
+      //response 데이터 json으로 변환하기
+      const jsonResult = await response.json(); //여기서부터 오류 null값..
+      if(jsonResult.result !== 'success'){
+        throw new Error(`${jsonResult.result} ${jsonResult.message}`);
+        }
+        setProjects([...projects,jsonResult.data]);
+        setRend(true);
+        modalFalse();
+    }
+    fetchfun();
+    }
   return (
     <Card>
       <FalconCardHeader title="내 프로젝트">
@@ -83,6 +208,8 @@ const Events = () => {
           </Form>
         )} */}
         <Button color='info' onClick={ () => { setModalIsOpen(true) }}>프로젝트 생성</Button>
+        
+        {/* 프로젝트 생성 모달 창 */}
         <Modal
           isOpen = {modalIsOpen}
           onRequestClose = { () => setModalIsOpen(false)}
@@ -90,21 +217,30 @@ const Events = () => {
           className={ styles.Modal }
           overlayClassName={ styles.Overlay }  
           contentLabel='프로젝트 생성'
+          searchMembers={searchMembers}
+          setSearchMembers={setSearchMembers}
           >
-          <div className='modal-content'> 
+          <div className='modal-content p-3'> 
             <headers className="bg-light d-flex flex-between-center border-bottom-0">
-              <h1>프로젝트 생성 모달창</h1>
-              <button onClick={ () => setModalIsOpen(false)}> Close</button>
+              <h1>프로젝트 생성</h1>
+              <button onClick={ () => modalFalse() }> Close</button>
             </headers>
             
-            <body>
-            <FormGroup>
+            <body className='p-2'>
+          <FormGroup>
             <Label className="fs-0" for="eventTitle">
               제목
             </Label>
-            <Input name="scheduleContents" id="eventTitle"><Link to='{}'/></Input>
+            <Input name="projectTitle" id="eventTitle" required onChange={({ target }) => modalhandleChange(target)} ></Input>
           </FormGroup>
 
+          <FormGroup>
+            <Label className="fs-0" for="eventTitle">
+              내용
+            </Label>
+            <Input name="projectDesc" id="eventTitle" required onChange={({ target }) => modalhandleChange(target)} ></Input>
+          </FormGroup>
+          
           <FormGroup>
             <Label className="fs-0" for="eventStart">
               시작일
@@ -120,8 +256,8 @@ const Events = () => {
                   setStartDate(dateTime);
                   let date = {};
                   date.value = dateTime.format('YYYY-MM-DD HH:mm:ss');
-                  date.name = 'scheduleStart';
-                  handleChange(date);
+                  date.name = 'projectStart';
+                  modalhandleChange(date);
                 }
               } //onChange 끝
               
@@ -145,8 +281,8 @@ const Events = () => {
                   setEndDate(dateTime);
                   let date = {};
                   date.value = dateTime.format('YYYY-MM-DD HH:mm:ss');
-                  date.name = 'scheduleEnd';
-                  handleChange(date);
+                  date.name = 'projectEnd';
+                  modalhandleChange(date);
                 }
               }}
               dateFormat="YYYY-DD-MM HH:mm:ss"
@@ -156,10 +292,52 @@ const Events = () => {
 
           <FormGroup>
             <h3>멤버 추가</h3>
-            <button>+</button> <input id="member" name="meber" onChange={ () => {}}/>
+            <button onClick={ () => {
+              memberSearchandInput();
+            }} className='mr-3 '>
+              +</button>
+              {
+              memberInputOpen == false ?
+              null
+              :
+              <input  id="member" name="meber" placeholder='멤버 찾기' value={keyword} onChange={ (e) => { let data = e.target.value; setKeyword(data)}}/>
+              }
+              {
+                !searchMembers ?
+                null:
+                //멤버 리스트 출력
+                <div>
+                {
+                  searchMembers
+                    .filter((searchMember)=> searchMember.userName.indexOf(keyword) !== -1 || searchMember.userEmail.indexOf(keyword) !== -1)
+                    .map(searchMember => <div onClick={ ()=>{ memberselect(searchMember.userNo, searchMember.userName, searchMember.userEmail, searchMember.userPhoto)}} 
+                    key={searchMember.userNo}><h6>{searchMember.userName}{' '}{searchMember.userEmail}</h6></div>)      
+                }
+                </div>
+              }
 
           </FormGroup>
 
+          <FormGroup>
+              <h3>추가된 멤버</h3>
+              {
+                !mSelects ?
+                null 
+                :
+                <div>
+                  {
+                    mSelects
+                      .map(mSelect => <div> <div>{mSelect.userName}{mSelect.userEmail}</div> </div>)
+                  }
+                </div>
+              }
+              
+              
+          </FormGroup>
+          
+          <FormGroup>
+              <Button style={{alignItems:'flex-center'}} onClick={ () => {projectAdd()}}>생성</Button>
+          </FormGroup>
             </body>
           </div>
         </Modal>
