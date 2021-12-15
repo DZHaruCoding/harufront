77f1f249e6e9568f99a3de259c003168e333dcfe
ghksxk1002 <ghksxk1002@naver.com>
2021-12-16 02:11:01 +0900

    #이승현 [FEAT] (회원가입폼) 발리데이션 추가

--- src/components/auth/RegistrationForm.js
+++ src/components/auth/RegistrationForm.js
@@ -1,48 +1,75 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { Button, CustomInput, Form, FormGroup, Input, Label } from 'reactstrap';
-// import Divider from '../common/Divider';
-// import SocialAuthButtons from './SocialAuthButtons';
 import withRedirect from '../../hoc/withRedirect';
-// import ForgetPassword from './split/ForgetPassword';
-// import {localIp} from '../../config';
+import '../../assets/scss/FormBox.scss';
+
 
 const RegistrationForm = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => {
+
   // State
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  // 발리데이션을 위한 상태 저장 
-  const [nameErrorText, setNameErrorText] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  // const [confirmPassword, setConfirmPassword] = useState('');
-  // const [isAccepted, setIsAccepted] = useState(false);
   const [isDisabled, setIsDisabled] = useState(true);
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
+  // 오류 메세지 출력을 위한 상태값
+  const [nameMessage, setNameMessage] = useState('')
+  const [emailMessage, setEmailMessage] = useState('')
+  const [passwordMessage, setPasswordMessage] = useState('')
 
-  const validateName = () => {
-    if (!name) {
-      setNameErrorText('이름을 입력해주세요.');
-    } else if (name.length < 2) {
-      setNameErrorText('이름은 최소 1자 이상입니다.');
+  // 유효성 검사를 위한 상태저장
+  const [isName, setIsName] = useState(false)
+  const [isEmail, setIsEmail] = useState(false)
+  const [isPassword, setIsPassword] = useState(false)
+  // const [isAccepted, setIsAccepted] = useState(false);
+  // const [confirmPassword, setConfirmPassword] = useState('');
+
+  const onChangeEmail = e => {
+    setEmail(e.target.value);
+    const emails = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    console.log("이메일 : " +email);
+    if (emails.test(email)) {
+      setEmailMessage('올바른 이메일 형식입니다')
+      setIsEmail(true)
     } else {
-      setNameErrorText('');
+      setEmailMessage('이메일 형식이 올바르지 않습니다')
+      setIsEmail(false)
     }
   };
+
+  const onChangeName = e => {
+    setName(e.target.value);
+    console.log("이름 : " + name)
+    if (name.length < 2) {
+      setNameMessage('2글자 이상 입력해주세요')
+      setIsName(false)
+    } else {
+      setNameMessage('올바른 이름 형식입니다')
+      setIsName(true)
+    }
+  };
+  const onChangePaswword = e => {
+    setPassword(e.target.value);
+    const passwords = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/
+    if (!passwords.test(password)) {
+      setPasswordMessage('특수문자, 문자, 숫자를 포함한 8~15 자리 이상이여야 합니다');
+      setIsPassword(false);
+    } else {
+      setPasswordMessage("안정한 비밀번호 입니다");
+      setIsPassword(true)
+    }
+
+  };
+
 
   // Handler
   const handleSubmit = async e => {
     // 새로고침 방지를 위함
     e.preventDefault();
-    // Inpur 값 검증
-    validateName();
+
     const json = {
       userEmail: email,
       userPassword: password,
@@ -74,33 +101,59 @@ const RegistrationForm = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => 
     setRedirect(true);
   };
 
-  // isLoading 값이 바뀌면 실행
-  // useEffect(() => {
-  //   (async function () {
-  //     if (isLoading) {
-  //       if (!nameErrorText) {
-  //         await new Promise((r) => setTimeout(r, 2000));
-  //         toast({
-  //           title: `회원가입 되었습니다!`,
-  //           description: `${name}님 환영합니다!`,
-  //           status: 'success',
-  //           duration: 9000,
-  //           isClosable: true,
-  //         });
-  //       }
-  //       setIsLoading(false);
-  //     }
-  //   })();
-  // }, [isLoading]);
+  const findEmail = async ()=>{
+      console.log("확인할 email : "+email)
+      const emails = {userEmail : email}
+  
+      try {
+          const response = await fetch(`/haru/api/user/checkemail`, {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+            },
+            body: JSON.stringify(emails)
+          })
+          
+          if (!response.ok) {
+            throw new Error(`${response.status} ${response.statusText}`)
+          }
+          
+          const json = await response.json();
+          console.log(json);
 
+          console.log(json.data);
+          if (json.result !== 'success') {
+            throw json.message;
+          }
+
+          if(json.data) {
+            setEmailMessage("이미 가입된 이메일 입니다.") 
+            setIsEmail(false)
+          } else {
+            setEmailMessage("가입이 가능한 이메일 입니다") 
+          }
+            
+           
+
+        } catch (err) {
+          console.log(err);
+        }
+  }
+
+  useEffect(()=>{
+    if(isEmail){
+      findEmail();    
+    }
+  },[isEmail])
 
   useEffect(() => {
     setRedirectUrl(`/authentication/${layout}/login`);
   }, [setRedirectUrl, layout]);
 
   useEffect(() => {
-    setIsDisabled(!name || !email || !password);
-  }, [name, email, password]);
+    setIsDisabled(!isName || !isEmail ||!isPassword);
+  }, [isName, isEmail ,isPassword]);
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -110,16 +163,17 @@ const RegistrationForm = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => 
         <Input
           placeholder={!hasLabel ? 'Email address' : ''}
           value={email}
-          onChange={({ target }) => setEmail(target.value)}
-          type="email"
+          onChange={onChangeEmail}
+        // type="email"
         />
+        {email.length > 0 && <span className={`message ${isEmail ? 'success' : 'error'}`}>{emailMessage}</span>}
       </FormGroup>
       {/* 이름입력 */}
       <FormGroup>
         {hasLabel && <Label>Name</Label>}
-        <Input placeholder={!hasLabel ? 'Name' : ''} value={name} onChange={({ target }) => setName(target.value)} />
+        <Input placeholder={!hasLabel ? 'Name' : ''} value={name} onChange={onChangeName} />
+        {name.length > 0 && (<span className={`message ${isName ? 'success' : 'error'}`}>{nameMessage}</span>)}
       </FormGroup>
-
       {/* 비밀번호 입력 */}
       <div>
         <FormGroup>
@@ -127,9 +181,10 @@ const RegistrationForm = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => 
           <Input
             placeholder={!hasLabel ? 'Password' : ''}
             value={password}
-            onChange={({ target }) => setPassword(target.value)}
+            onChange={onChangePaswword}
             type="password"
           />
+          {password.length > 0 && (<span className={`message ${isPassword ? 'success' : 'error'}`}>{passwordMessage}</span>)}
         </FormGroup>
       </div>
       <FormGroup>
@@ -137,7 +192,7 @@ const RegistrationForm = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => 
         {/* <Button tag={Link} to="/authentication/basic/forget-password" color="primary" block className="mt-3">
           다음
         </Button> */}
-        <Button color="primary" block className="mt-3">
+        <Button color="primary" block className="mt-3" disabled={isDisabled} >
           다음
         </Button>
       </FormGroup>
