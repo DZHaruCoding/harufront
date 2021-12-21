b30a768f11b0827eff8f3ab2129ae48583aa7de8
kimhaengun <chxl89@naver.com>
2021-12-21 17:01:25 +0900

    #김행운[update]:events.js formgroup 수정

--- src/components/page/Events.js
+++ src/components/page/Events.js
@@ -1,5 +1,5 @@
-import React, { createContext, useEffect, useState } from 'react';
-import { Alert, Button, Card, CardBody, Col, CustomInput, Form, Modal, Row, FormGroup, Label, Input, NavLink, } from 'reactstrap';
+import React, {createContext, useEffect, useState} from 'react';
+import { Alert, Button, Card, CardBody, Col, CustomInput, Form, Modal, Row,FormGroup,Label,Input, NavLink, } from 'reactstrap';
 import EventSummary from '../event/EventSummary';
 import Loader from '../common/Loader';
 import FalconCardHeader from '../common/FalconCardHeader';
@@ -15,13 +15,13 @@ import { flexibleCompare } from '@fullcalendar/react';
 import Datetime from 'react-datetime';
 import { Link } from 'react-router-dom';
 import { offline } from 'is_js';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faCheckCircle, faTimesCircle, faAt, faCog, faTimes, faPlus } from '@fortawesome/free-solid-svg-icons';
-import withRedirect from '../../hoc/withRedirect';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBell, faCheckCircle, faTimesCircle, faAt, faCog, faTimes, faPlus} from '@fortawesome/free-solid-svg-icons';
 
-const Events = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => {
+
+const Events = () => {
   // const { loading, data: events } = useFakeFetch(rawEvents);
-  const [projects, setProjects] = useState([]);
+  const [projects,setProjects] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const [formObj, setFormObj] = useState();
@@ -34,86 +34,80 @@ const Events = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => {
     let name = target.name;
     let value = name === 'allDay' ? target.checked : target.value;
     setFormObj({ ...formObj, [name]: value });
-    console.log('모달창 데이터', formObj);
+    console.log('모달창 데이터',formObj);  
   };
   //useEffect에 async 바로 주지말고 함수 만들어서 함수에 async
-  useEffect(() => {
+  useEffect( () => {
     console.log('들어옴?');
-    const data = async () => {
-      try {
-        const response = await fetch('/haru/api/project/1', {
-          method: 'get',
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          },
-          redirect: 'follow', // manual, *follow, error
-          referrer: 'no-referrer', // no-referrer, *client
-          body: null
-        });
+    const data = async ()=>{ 
+    try {
+      console.log('authUserNo / fetch 수정해야함',window.sessionStorage.getItem("authUserNo"))
+      const response = await fetch('/haru/api/project/1',{
+        method: 'get',
+        headers:{
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        // redirect:'manual(response.url)',
+        body: null
+      });
 
-        //성공하면
-        console.log(response)
-        console.log(response.url);
-        if (!response.ok) {
-          window.location.href= response.url;
+      //성공하면
+      if(!response.ok){
           throw new Error(`${response.status} ${response.statusText}`);
-        }
-
-        //결과를 json으로 변환
-        const jsonResult = await response.json();
-        console.log(jsonResult.data);
-        //통신은 했지만 결과값이 success가 아니면 
-        if (jsonResult.result !== 'success') {
-          throw new Error(`${jsonResult.result} ${jsonResult.message}`);
-        }
-
-        // setEmails에 데이터 셋팅하기
-        setProjects(jsonResult.data);
-        setRend(false);
-
-      } catch (error) {
-        console.log(error);
       }
+      //결과를 json으로 변환
+      const jsonResult = await response.json();
+      console.log(jsonResult.data);
+      //통신은 했지만 결과값이 success가 아니면 
+      if(jsonResult.result !== 'success'){
+          throw new Error(`${jsonResult.result} ${jsonResult.message}`);
+      }
+
+      // setEmails에 데이터 셋팅하기
+      setProjects(jsonResult.data);
+      setRend(false);
+
+    } catch (error) {
+      console.log(error);
     }
-    data();
-  }, [rend]);
-
-
+  }
+  data();
+  },[rend]);
+  
   //멤버 인풋
   const [memberInputOpen, setMemberInputOpen] = useState(false);
-
   const [searchInputOpen,setSearchInputOpen] = useState(false);
   const [searchMembers, setSearchMembers] = useState(); 
 
   const memberSearchandInput = (memberInputOpen) => {    
     const fetchfun = async () => {
       try {
-        const response = await fetch('/haru/api/project/member', {
+        const response = await fetch('/haru/api/project/member',{
           method: 'get',
-          headers: {
+          headers:{
             'Content-Type': 'application/json',
             'Accept': 'application/json'
           },
           body: null
         });
 
-        console.log('프로젝트 생성 멤버 찾기 @: ' + response);
+        console.log('프로젝트 생성 멤버 찾기 @: '+response);
 
 
-        if (!response.ok) {
+      if(!response.ok){
           throw new Error(`${response.status} ${response.statusText}`);
-        }
-        //response 데이터 json으로 변환하기
-        const jsonResult = await response.json(); //여기서부터 오류 null값..
+      }
+      //response 데이터 json으로 변환하기
+      const jsonResult = await response.json(); //여기서부터 오류 null값..
+     
+      console.log('프로젝트 생성 멤버 찾기 json data :',jsonResult.data);
 
-        console.log('프로젝트 생성 멤버 찾기 json data :', jsonResult.data);
-
-        //통신 했지만 결과값이 success가 아니면
-        if (jsonResult.result !== 'success') {
-          throw new Error(`${jsonResult.result} ${jsonResult.message}`);
-        }
-        setSearchMembers(jsonResult.data);
+            //통신 했지만 결과값이 success가 아니면
+      if(jsonResult.result !== 'success'){
+      throw new Error(`${jsonResult.result} ${jsonResult.message}`);
+      }
+      setSearchMembers(jsonResult.data);
 
       } catch (error) {
         console.log(error);
@@ -130,12 +124,12 @@ const Events = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => {
 
   }
   //키워드
-  const [keyword, setKeyword] = useState('');
+  const [keyword,setKeyword] = useState('');
   //선택 된 멤버 담는 상태값
   const [mSelects, setMSelects] = useState([]);
   //멤버 클릭 시
-  const memberselect = (userNo, userName, userEmail, userPhoto) => {
-    console.log("클릭된 유저 no :", userNo, userName, userEmail, userPhoto)
+  const memberselect = (userNo,userName,userEmail,userPhoto) =>{
+    console.log("클릭된 유저 no :",userNo,userName,userEmail,userPhoto)
     const data = {
       userNo: userNo,
       userName: userName,
@@ -145,39 +139,40 @@ const Events = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => {
 
     //멤버 클릭 중복 제거
     let a = false;
-    for (let i = 0; i < mSelects.length; i++) {
-      if (data.userNo == mSelects[i].userNo) {
-
+    for(let i = 0; i<mSelects.length; i++){
+      if(data.userNo == mSelects[i].userNo){
+        
         a = true;
         return;
       }
     }
-    if (a == true) {
+    if(a==true){
       return;
     }
     setMSelects([...mSelects, data]);
   }
 
-  const modalFalse = () => {
+  const modalFalse = () =>{
     setModalIsOpen(false)
     setSearchMembers()
     setKeyword('');
     setMSelects([]);
     setMemberInputOpen(false);
+    setRend(true);
   }
   //프로젝트 모달창 input태그 오브젝트로 변환
   const [modalformObj, setModalFormObj] = useState();
-  const modalhandleChange = (target) => {
+  const modalhandleChange = (target) =>{
     let name = target.name;
     let value = name === 'allDay' ? target.checked : target.value;
     setModalFormObj({ ...modalformObj, [name]: value });
   }
 
   //프로젝트 생성 클릭 시
-  const projectAdd = () => {
-    console.log('modalobj:', modalformObj)
+  const projectAdd = () =>{
+    console.log('modalobj:',modalformObj)
     //members
-    console.log('finalobj', mSelects);
+    console.log('finalobj',mSelects);
 
 
     modalformObj.members = mSelects;
@@ -190,31 +185,29 @@ const Events = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => {
     // console.log('만든 members',members);
     // setModalFormObj({...modalformObj, members});
     console.log(modalformObj);
-    const fetchfun = async () => {
-      const response = await fetch('/haru/api/project/add/1', {
-        method: "post",
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
-        body: JSON.stringify(modalformObj)
-      });
-      console.log();
-      if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`);
+    const fetchfun = async() => {
+        const response = await fetch('/haru/api/project/add/1',{
+          method:"post",
+          headers:{
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+          },
+          body:JSON.stringify(modalformObj)
+        });
+        if(!response.ok){
+          throw new Error(`${response.status} ${response.statusText}`);
       }
       //response 데이터 json으로 변환하기
       const jsonResult = await response.json(); //여기서부터 오류 null값..
-      if (jsonResult.result !== 'success') {
+      if(jsonResult.result !== 'success'){
         throw new Error(`${jsonResult.result} ${jsonResult.message}`);
-      }
-      setProjects([...projects, jsonResult.data]);
-      setRend(true);
-      modalFalse();
+        }
+        setProjects([...projects,jsonResult.data]);
+        
+        modalFalse();
     }
     fetchfun();
-  }
-
+    }
 
     const rending = (render) =>{
       if(render === true){
@@ -222,9 +215,7 @@ const Events = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => {
       }else{
         setRend(false);
       }
-
     }
-  }
   return (
     <Card>
       <FalconCardHeader title="내 프로젝트">
@@ -239,7 +230,6 @@ const Events = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => {
             </CustomInput>
           </Form>
         )} */}
-
         <Button color='info' onClick={ () => { setModalIsOpen(true) }}>프로젝트 생성</Button>
         
  
@@ -255,101 +245,70 @@ const Events = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => {
           contentLabel='프로젝트 생성'
           searchMembers={searchMembers}
           setSearchMembers={setSearchMembers}
-        >
-          <div className='modal-content p-3'>
+          >
+          <div className='modal-content p-3'> 
             <headers className="d-flex flex-between-center border-bottom-0">
               <h1>프로젝트 생성</h1>
-              <button style={{ backgroundColor: "white", border: "0px", marginTop: "-10px" }} onClick={() => modalFalse()}> <FontAwesomeIcon style={{ fontSize: "25px" }} icon={faTimes} /></button>
+              <button style={{backgroundColor:"white", border:"0px", marginTop:"-10px"}} onClick={ () => modalFalse() }> <FontAwesomeIcon style={{fontSize:"25px"}} icon={faTimes}/></button>
             </headers>
-
+            
             <body className='p-2'>
-              <FormGroup>
-                <Label className="fs-0" for="eventTitle">
-                  제목
-                </Label>
-                <Input name="projectTitle" id="eventTitle" required onChange={({ target }) => modalhandleChange(target)} ></Input>
-              </FormGroup>
+          <FormGroup>
+            <Label className="fs-0" for="eventTitle">
+              제목
+            </Label>
+            <Input name="projectTitle" id="eventTitle" required onChange={({ target }) => modalhandleChange(target)} ></Input>
+          </FormGroup>
 
-              <FormGroup>
-                <Label className="fs-0" for="eventTitle">
-                  내용
-                </Label>
-                <Input name="projectDesc" id="eventTitle" required onChange={({ target }) => modalhandleChange(target)} ></Input>
-              </FormGroup>
-
-              <FormGroup>
-                <Label className="fs-0" for="eventStart">
-                  시작일
-                </Label>
-                {/* 시작일 날짜 */}
-
-                <Datetime
-                  timeFormat={true}
-                  //addScheduleStartDate = 부모에서 모달창 클릭 했을때 받아오는 날짜 데이터
-                  value={startDate}
-                  onChange={dateTime => {
-                    if (dateTime._isValid) {
-                      setStartDate(dateTime);
-                      let date = {};
-                      date.value = dateTime.format('YYYY-MM-DD HH:mm:ss');
-                      date.name = 'projectStart';
-                      modalhandleChange(date);
-                    }
-                  } //onChange 끝
-
-                  }
-
-                  dateFormat="YYYY-DD-MM HH:mm:ss"
-                  inputProps={{ placeholder: 'YYYY-MM-DD HH:mm:ss', id: 'eventStart' }}
-                />
-              </FormGroup>
-
-              <FormGroup>
-                <Label className="fs-0" for="eventEnd">
-                  마감일
-                </Label>
-                {/* 마감일 날짜 */}
-                <Datetime
-                  value={endDate}
-                  timeFormat={true}
-                  onChange={dateTime => {
-                    if (dateTime._isValid) {
-                      setEndDate(dateTime);
-                      let date = {};
-                      date.value = dateTime.format('YYYY-MM-DD HH:mm:ss');
-                      date.name = 'projectEnd';
-                      modalhandleChange(date);
-                    }
-                  }}
-                  dateFormat="YYYY-DD-MM HH:mm:ss"
-                  inputProps={{ placeholder: 'YYYY-DD-MM HH:mm:ss', id: 'eventEnd' }}
-                />
-              </FormGroup>
-
-              <FormGroup>
-                <h3>멤버 추가</h3>
-                <button style={{ backgroundColor: "#EDF2F9", border: "0px" }} onClick={() => {
-                  memberSearchandInput();
-                }} className='mr-3 '>
-                  <FontAwesomeIcon style={{ color: "#27BCFD", fontSize: "25px" }} icon={faPlus} /></button>
-                {
-                  memberInputOpen == false ?
-                    null
-                    :
-                    <input id="member" name="meber" placeholder='멤버 찾기' value={keyword} onChange={(e) => { let data = e.target.value; setKeyword(data) }} />
+          <FormGroup>
+            <Label className="fs-0" for="eventTitle">
+              내용
+            </Label>
+            <Input name="projectDesc" id="eventTitle" required onChange={({ target }) => modalhandleChange(target)} ></Input>
+          </FormGroup>
+          
+          <FormGroup>
+            <Label className="fs-0" for="eventStart">
+              시작일
+            </Label>
+            {/* 시작일 날짜 */}
+          
+            <Datetime
+              timeFormat={true}
+              //addScheduleStartDate = 부모에서 모달창 클릭 했을때 받아오는 날짜 데이터
+              value={ startDate }
+              onChange={dateTime => {
+                if (dateTime._isValid) {
+                  setStartDate(dateTime);
+                  let date = {};
+                  date.value = dateTime.format('YYYY-MM-DD HH:mm:ss');
+                  date.name = 'projectStart';
+                  modalhandleChange(date);
                 }
-                {
-                  !searchMembers ?
-                    null :
-                    //멤버 리스트 출력
-                    <div>
-                      {
-                        searchMembers
-                          .filter((searchMember) => searchMember.userName.indexOf(keyword) !== -1 || searchMember.userEmail.indexOf(keyword) !== -1)
-                          .map(searchMember => <div onClick={() => { memberselect(searchMember.userNo, searchMember.userName, searchMember.userEmail, searchMember.userPhoto) }}
-                            key={searchMember.userNo}><h6>{searchMember.userName}{' '}{searchMember.userEmail}</h6></div>)
-                      }
-                    </div>
+              } //onChange 끝
+              
+            }
+              
+              dateFormat="YYYY-DD-MM HH:mm:ss"
+              inputProps={{ placeholder: 'YYYY-MM-DD HH:mm:ss', id: 'eventStart' }}
+            />
+          </FormGroup>
+      
+          <FormGroup>
+            <Label className="fs-0" for="eventEnd">
+              마감일
+            </Label>
+            {/* 마감일 날짜 */}
+            <Datetime
+              value={endDate}
+              timeFormat={true}
+              onChange={dateTime => {
+                if (dateTime._isValid) {
+                  setEndDate(dateTime);
+                  let date = {};
+                  date.value = dateTime.format('YYYY-MM-DD HH:mm:ss');
+                  date.name = 'projectEnd';
+                  modalhandleChange(date);
                 }
               }}
               dateFormat="YYYY-DD-MM HH:mm:ss"
@@ -382,13 +341,31 @@ const Events = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => {
                     .map(searchMember => <div style={{borderBottom:"1px dashed"}} onClick={ ()=>{ memberselect(searchMember.userNo, searchMember.userName, searchMember.userEmail, searchMember.userPhoto)}} 
                     key={searchMember.userNo}><span style={{fontWeight:"bold", color:"rgb(94,110,130)"}}>{searchMember.userName}</span>{' '}<span>{searchMember.userEmail}</span></div>)      
                 }
+                </div>
+              }
 
+          </FormGroup>
 
-              </FormGroup>
-
-              <FormGroup style={{ display: "flex", justifyContent: "center" }}>
-                <Button style={{ backgroundColor: "white", color: "black" }} onClick={() => { projectAdd() }}>생성</Button>
-              </FormGroup>
+          <FormGroup>
+              <h3>추가된 멤버</h3>
+              {
+                !mSelects ?
+                null 
+                :
+                <div>
+                    {
+                      mSelects
+                        .map(mSelect => <div> <div>{mSelect.userName}{mSelect.userEmail}</div> </div>)
+                    }
+                  </div>
+              }
+              
+              
+          </FormGroup>
+          
+          <FormGroup style={{display:"flex", justifyContent:"center"}}>
+              <Button style={{backgroundColor:"white", color:"black"}} onClick={ () => {projectAdd()}}>생성</Button>
+          </FormGroup>
             </body>
           </div>
         </Modal>
@@ -403,7 +380,7 @@ const Events = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => {
         {/* {loading ? (
           <Loader />
         ) : isIterableArray(events) ? ( */}
-        <EventSummary projects={projects} rendcallback={rending} deletecallback={rending} />
+          <EventSummary  projects={projects} rendcallback={rending} deletecallback={rending}/>
         {/* ) : (
           <Alert color="info" className="mb-0">
             No events found!Fpro
@@ -414,4 +391,4 @@ const Events = ({ setRedirect, setRedirectUrl, layout, hasLabel }) => {
   );
 };
 
-export default withRedirect(Events);
+export default Events;
